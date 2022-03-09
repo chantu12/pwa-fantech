c35849cf9e6c4f82bc092b0700c6b100d5dd1939
andrei12 <andrei.stoian@fintechos.com>
2022-03-09 14:16:57 +0200

    fix sper

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ var path = require('path');
 var express = require('express');
 
 var app = express();
+var distDir = __dirname + '/dist/';
 
-app.use(express.static(path.join(__dirname, './dist')));
+app.use(express.static(distDir));
+
 app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function () {
