c0b2a55ba5aff1a2b7006db8341d9b4367d46a98
andrei12 <andrei.stoian@fintechos.com>
2022-03-09 14:42:04 +0200

    sw webpack plugin

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
-/* app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-}); */
+
 app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function () {
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './index.js',
@@ -26,6 +27,12 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './index.html',
       filename: './index.html',
+    }),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
     }),
   ],
 };
